238c86460030713ac2d4bd9d52d9d9a9a6c8e69a
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-04-24 00:16:43 +0330

    Project Started && Navbar Completed

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+const Logo = () => {
+	const router = useRouter()
+
+	return (
+		<Image
+			src="/images/logo.png"
+			alt="logo"
+			width={100}
+			height={100}
+			onClick={() => router.push("/")}
+			className="hidden md:block cursor-pointer"
+		/>
+	)
+}
+
+export default Logo

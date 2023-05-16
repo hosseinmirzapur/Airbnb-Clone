38999dd468f6dc4101274c981988ee2a2aa6d3e0
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-05-16 23:14:18 +0330

    updated next.config.js for user's google account photo

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
 		appDir: true,
 	},
 	images: {
-		domains: ["avatars.githubusercontent.com", "res.cloudinary.com"],
+		domains: [
+			"avatars.githubusercontent.com",
+			"res.cloudinary.com",
+			"lh3.googleusercontent.com",
+		],
 	},
 }
 

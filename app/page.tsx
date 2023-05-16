6e77d2923c007c636d37c6663c444a1f849875ed
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-05-16 22:43:07 +0330

    deployment testing #3

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ import ListingCard from "./components/listings/ListingCard"
 import { getCurrentUser } from "./actions/getCurrentUser"
 import { SafeListing } from "./types"
 
-const Home = async (searchParams: IListingsParams) => {
+interface HomeProps {
+	searchParams: IListingsParams
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
 	const listings: SafeListing[] = await getListings(searchParams)
 
 	const currentUser = await getCurrentUser()
